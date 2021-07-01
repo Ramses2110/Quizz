a12c654445c5050c315a68ed2114792a58c534ac
Ramses_Vega <rmanuelvh@gmail.com>
2021-06-30 22:03:18 -0600

    Este es el quizz

---
+++ app.js
@@ -0,0 +1,16 @@
+//arrow funtion
+const cambiarWlWightAlTextoDelosLi = () =>{
+    var ol = document.getElementsByClassName("none")
+    var ol2 = document.querySelector('.none')
+
+    ol[0].ClassList.add('negrita')
+}
+const cambiarTextoYTamanAP =() =>{
+    var p =document.getElementsByTagName('p')
+    //var p = querySelector('p')
+    p[0].classList.add('letraTam')
+    p[1].classList.add('letraTam')
+    p[2].classList.add('letraTam')
+    p[3].classList.add('letraTam')
+}
+
